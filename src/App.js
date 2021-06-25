6aa38814a3b933d6796c5ac652fcc48d46235adf
Jonathan <jbarrong@gmail.com>
2021-06-25 11:24:40 +0200

    bunk

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ function App() {
   
   const [destination, setDestination] = useState('VLC');
   const [originDestination, setOriginDestination] = useState('PRG');
+  const [direct, setOriginDirect] = useState('PRG');
 
   async function fetchDestination() {
-    const results = await fetch(`https://api.skypicker.com/flights?flyFrom=${originDestination}&to=${destination}&dateFrom=26/06/2021&dateTo=26/06/2021&partner=data4youcbp202106&v=3&limit=5&sort=date&asc=1`);
+    const results = await fetch(`https://api.skypicker.com/flights?flyFrom=${originDestination}&to=${destination}&dateFrom=26/06/2021&dateTo=26/06/2021&partner=data4youcbp202106&v=3&limit=5&sort=date&asc=1&direct_flights=${direct}`);
     const data = await results.json();
     console.log(data);
     setFlights(data);
