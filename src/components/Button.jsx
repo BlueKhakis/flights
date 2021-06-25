7a2fa92120cf969810dc5ff413f931159b427ce4
Jonathan <jbarrong@gmail.com>
2021-06-25 11:15:29 +0200

    button

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+function DetailButton(props)
+{
+    function handleSubmit(event)
+    {
+        event.preventDefault()
+        
+        window.location.href = `/${props.index}`; console.log('string')
+    }
+
+    return (
+        <button onClick={(event) => handleSubmit(event)}> Detail</button >
+            
+        
+    )
+}
+
+export default DetailButton;
